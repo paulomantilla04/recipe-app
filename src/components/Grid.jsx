72cbfe8f94e7556b030a1faecb7ae66e1a58b56f
paulomantilla04 <ma475671@uaeh.edu.mx>
2024-02-26 20:27:41 -0600

    Link añadido

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { GaugeIcon } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
-const GridItem = ({ image, title, difficulty, isDouble }) => {
+const GridItem = ({ image, title, difficulty, isDouble, to }) => {
     const boxStyle = 'bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-end transition-all duration-300 relative'; 
     const contentStyle = 'opacity-0 transition-opacity duration-300';
     const overlayStyle = 'rounded-xl absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 hover:opacity-70 transition-all duration-300';
 
     return (
-        <button className={`${boxStyle} ${isDouble ? 'col-span-2' : ''} hover:scale-95`} style={{backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+        <Link to={to} className={`${boxStyle} ${isDouble ? 'col-span-2' : ''} hover:scale-95`} style={{backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
             <div className={overlayStyle}></div>
             <h2 className={`z-10 text-md text-white font-bold font-primary ${contentStyle}`}>{title}</h2>
             <p className={`z-10 inline-flex gap-1 items-center text-xs font-semibold text-white font-primary ${contentStyle}`}>
                 <GaugeIcon/>
                 {difficulty}
             </p>
-        </button>
+        </Link>
     );
 }
 
@@ -22,7 +23,7 @@ const Grid = ({ data }) => {
     return (
         <div className='flex flex-col md:grid md:grid-cols-3 auto-rows-[250px] gap-4 my-10'>
             {Object.keys(data).map((key, i) =>
-                <GridItem key={i} image={data[key].image} title={data[key].title} difficulty={data[key].difficulty} isDouble={i === 3} />
+                <GridItem to={`${i+1}`}key={i} image={data[key].image} title={data[key].title} difficulty={data[key].difficulty} isDouble={i === 3} />
             )}
         </div>
     );
--- src/pages/Meats.jsx
+++ src/pages/Meats.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from '../components/Container';
 import Grid from '../components/Grid';
 import data from '../data';
 
@@ -7,9 +6,7 @@ const Meats = () => {
   return (
     <div className='flex flex-col items-center justify-center py-28'>
       <h1 className='font-primary text-3xl font-extrabold'>CARNES</h1>
-      <Container>
         <Grid data={data} />
-      </Container>
     </div>
   );
 }
